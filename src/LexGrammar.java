a37e5c3c62ed8e0045320ce27d919ba6eacc0884
Vyacheslav Vasiliev <slavav99@mail.ru>
2019-09-12 22:42:37 +0300

    Now it works

--- src/LexGrammar.java
+++ src/LexGrammar.java
@@ -1,8 +1,19 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class LexGrammar {
     HashMap<String, String> grammar;
+
+    LexGrammar() throws FileNotFoundException {
+        grammar = new HashMap<>(300);
+        Scanner input = new Scanner(new File("grammarSwift.txt"));
+        String[] temp;
+        while (input.hasNextLine()) {
+            temp = input.nextLine().split(" ");
+            grammar.put(temp[0], temp[1]);
+        }
+    }
 
     LexGrammar(String path) {
         grammar = new HashMap<>(300);
@@ -24,5 +35,16 @@ public class LexGrammar {
 
     public String putIfAbsent (String key, String val) {
         return grammar.putIfAbsent(key, val);
+    }
+
+    public void print () {
+        for (Map.Entry<String, String> entry : grammar.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        LexGrammar lexGrammar = new LexGrammar();
+        lexGrammar.print();
     }
 }
