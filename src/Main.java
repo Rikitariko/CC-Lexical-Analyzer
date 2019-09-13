701c3e4e0fff93f78900e67a8c2ec0a79a7ca643
Irek Nazmiev <ireknazmievirek@gmail.com>
2019-09-13 15:54:11 +0300

    Implement Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        LexParser lexParser = new LexParser("input.txt");
+        LexGrammar lexGrammar = new LexGrammar();
+        LexAnal lexAnal = new LexAnal(lexGrammar);
+
+        HashMap token;
+        String lexeme;
+
+        while (true) {
+            lexeme = lexParser.getLexeme();
+            if (lexeme.equals(""))
+                break;
+            else {
+                token = lexAnal.getToken(lexeme);
+
+                System.out.print("'" + lexeme + "' -> ");
+                System.out.println(token.toString());
+            }
+        }
     }
 }
