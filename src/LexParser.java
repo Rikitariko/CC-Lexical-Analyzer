0ee3560e1048479c0d344a361183349bc863b476
Rikitariko <33659442+Rikitariko@users.noreply.github.com>
2019-09-13 14:13:27 +0300

    Update LexParser.java

--- src/LexParser.java
+++ src/LexParser.java
@@ -1,66 +1,107 @@
-import LexGrammar.java;
-import java.util.*;
+import java.io.*;
+import java.io.File;
+import java.util.Scanner;
+
+import static java.lang.Integer.min;
 
 public class LexParser {
     private static Scanner in;
     private static LexGrammar grammar;
     private static String lastString;
     private static int id;
+    private static boolean flag;
 
-    Starter(String s) {
-        Scanner in = new Scanner(new File(s));
-        grammar = new LexGrammar('...'); // дописать
-        lastString = "";
+    LexParser(/*String s*/) throws Exception {
+        in = new Scanner(new File("input.txt"));
+        grammar = new LexGrammar(); // дописать
+        if (in.hasNextLine())
+            lastString = in.nextLine();
+        else
+            lastString = "";
         id = 0;
+        flag = false;
     }
 
-    private boolean isLetter(char ch) {
+    private static boolean isLetter(char ch) {
         return ((int)ch >= (int)'a' && (int)ch <= (int)'z') || ((int)ch >= (int)'A' && (int)ch <= (int)'Z');
     }
 
-    private boolean isNumber(char ch) {
+    private static boolean isNumber(char ch) {
         return ((int)ch >= (int)'0' && (int)ch <= (int)'9');
     }
 
-    public String getLexeme() {
+    public static String getLexeme() {
         String ans = "";
-        if (this.lastString == "" || this.id == this.lastString.length()) {
-            this.lastString = this.in.nextLin();
+
+        if (flag)
+            return "";
+
+        if (lastString.isEmpty()) {
             id = 0;
+            if (in.hasNextLine())
+                lastString = in.nextLine();
+            else flag = true;
+
+            return "\\n";
         }
 
-        switch (id) {
-            case (isLetter(lastString[id])):
-                for (int i = id; i < lastString.length(); i++) {
-                    if !(isLetter(lastString[i]) || isNumber(lastString[i]))
-                        break;
-                    ans += lastString[i];
-                    continue;
+        if (id == lastString.length()) {
+            id = 0;
+            if (in.hasNextLine())
+                lastString = in.nextLine();
+            else flag = true;
+
+            return "\\n";
+        }
+
+        int i;
+        if (isLetter(lastString.charAt(id))) {
+            for (i = id; i < lastString.length(); i++) {
+                if (!(isLetter(lastString.charAt(i)) || isNumber(lastString.charAt(i)) || lastString.charAt(i) == '_'))
+                    break;
+                ans = ans + lastString.charAt(i);
+            }
+            id = i;
+        } else if (isNumber(lastString.charAt(id))) {
+            boolean flDot = false;
+
+            for (i = id; i < lastString.length(); i++) {
+                if (lastString.charAt(i) == '.' && !flDot && (i + 1 < lastString.length() && isNumber(lastString.charAt(i + 1))))
+                    flDot = true;
+                else if (!(isNumber(lastString.charAt(i))))
+                    break;
+
+                ans = ans + lastString.charAt(i);
+            }
+            id = i;
+        } else {
+            String sampleStr = "";
+            int constLen = 20;
+            if (lastString.charAt(id) == ' ') {
+                id++;
+                return " ";
+            }
+            for (i = id; i < min(id + constLen, lastString.length()); i++) {
+                sampleStr = sampleStr + lastString.charAt(i);
+                if (grammar.contains(sampleStr)) {
+                    id = i + 1;
+                    ans = sampleStr;
                 }
-                id = i;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) throws Exception {
+        LexParser ls = new LexParser();
+        while (true) {
+            String s = getLexeme();
+            if (s.equals(""))
                 break;
-            case (isNumber(lastString[id])):
-                for (int i = id; i < lastString.length(); i++) {
-                    if !(isNumber(lastString[i]))
-                        break;
-                    ans += lastString[i];
-                    continue;
-                }
-                id = i;
-                break;
-            default:
-                String sampleStr = "";
-                constLen = 4;
-                for (int i = id; i < min(id + constLen, lastString.length()); i++) {
-                    if !(isLetter(lastString[i]) || isNumber(lastString[i])) {
-                        sampleStr = sampleStr + lastString[i];
-                        if (grammar.contain(sampleStr)) {
-                            id = i + 1;
-                            ans = sampleStr;
-                        }
-                    }
-                }
-                break;
+            else
+                if (s.equals("\\n"))
+                    System.out.println("<" + s + ">");
+                else System.out.print("<" + s + ">");
+
         }
     }
 }
